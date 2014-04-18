4909da7f6c6d612695eb9dcc4c4b10796ad622af
Fission Xuiptz <fissionxuiptz@softwaremojo.com>
2014-04-18 19:38:05 +1000

    v1.0.1
    Minor style changes

--- lib/reverie.rb
+++ lib/reverie.rb
@@ -10,7 +10,7 @@ class Reverie
 
   DH_URI = URI 'https://api.dreamhost.com/'
   IP_URI = URI 'http://myexternalip.com/raw'
-  VERSION = '1.0.0'
+  VERSION = '1.0.1'
 
   Settings.define :conf, type: :filename, description: 'The location of the configuration file',
                   default: Configliere::DEFAULT_CONFIG_LOCATION[:user].call('reverie')
@@ -38,10 +38,11 @@ class Reverie
 
   def update_dns
     t = Time.now - (@options[:updated_at] || Time.mktime(0))
-    @log.debug "too soon, updated #{ t.to_int }s ago" and return unless t > 900
-    @log.debug 'get_ip failed'                        and return unless ip = get_ip
-    @log.debug "not updating #{ @options[:record] }"  and return unless ip != @options[:ip]
-    @log.debug 'replace_record failed'                and return unless replace_record @options[:record], ip
+    return if
+      (t < 900                               and @log.debug "too soon, updated #{ t.to_int }s ago") ||
+      ((ip = get_ip).nil?                    and @log.debug 'get_ip failed') ||
+      (ip == @options[:ip]                   and @log.debug "not updating #{ @options[:record ]}") ||
+      (!replace_record @options[:record], ip and @log.debug 'replace_record failed')
 
     @options.merge! ip: ip, updated_at: Time.now
     @options.save! @conf
@@ -54,11 +55,11 @@ class Reverie
     status, res = api_call :list_records
     return false unless status == 'success'
 
-    res.find { |r| r['record'] == record && r['editable'] == 1 }.tap { |r|
+    res.detect { |r| r['record'] == record && r['editable'] == 1 }.tap { |r|
       api_call :remove_record, record: record, type: r['type'], value: r['value'] if r
     }
 
-    status, res = api_call :add_record, record: record, type: 'A', value: ip, comment: 'Reverie'
+    status, res = api_call :add_record, record: record, type: 'A', value: ip, comment: "Reverie (#{ Time.now })"
     status == 'success'
   end
 
@@ -66,7 +67,7 @@ class Reverie
     ip = Net::HTTP.get_response(IP_URI).body.chomp
     @log.debug "get_ip found #{ ip }"
     ip if ip =~ Resolv::IPv4::Regex
-  rescue Net::ReadTimeout => e
+  rescue Net::ReadTimeout
     @log.warn 'IP lookup timed out'
   end
 
