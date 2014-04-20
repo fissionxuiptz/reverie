7babe94b1b65cfad35aae5387d6a45ba5954b20c
Fission Xuiptz <fissionxuiptz@softwaremojo.com>
2014-04-20 17:37:02 +1000

    Styled according to rubocop

--- lib/reverie/mojo.rb
+++ lib/reverie/mojo.rb
@@ -2,19 +2,20 @@ Reverie.class_eval do
   SM_URI = URI 'http://softwaremojo.com/'
 
   def get_ip
-    @log.debug "getting IP"
-    t = Time.now.utc
-    c = Digest::SHA256.hexdigest "#{ t.strftime('%y%m%d%H') }get_ip#{ @options[:challenge] }"
-    r = Digest::SHA256.hexdigest "#{ t.strftime('%y%m%d%H') }get_ip#{ @options[:response] }"
+    d 'getting IP'
+    s = "#{ Time.now.utc.strftime '%y%m%d%H' }get_ip%s"
+    c = Digest::SHA256.hexdigest(s % Settings[:challenge])
+    r = Digest::SHA256.hexdigest(s % Settings[:response])
 
-    SM_URI.query = URI.encode_www_form key: c
-    @log.debug "challenge: #{ SM_URI.query }"
+    SM_URI.query = URI.encode_www_form(key: c)
+    d :kv, 'challenge', SM_URI.query
 
-    res = YAML.load(Net::HTTP.start(SM_URI.host) { |http| http.get SM_URI }.body)
-    @log.debug "response: #{ res }"
+    req = Net::HTTP.start(SM_URI.host) { |http| http.get SM_URI }
+    res = YAML.load(req.body)
+    d :kv, 'response', res
 
-    (r == res['key'] and res['ip'] =~ Resolv::IPv4::Regex) ? res['ip'] : nil
+    (r == res['key'] && res['ip'] =~ Resolv::IPv4::Regex) ? res['ip'] : nil
   rescue Net::ReadTimeout
-    @log.warn 'IP lookup timed out'
+    w :timeout, 'IP lookup', SM_URI
   end
 end
