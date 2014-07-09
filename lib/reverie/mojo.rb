17e263db7db602ea4b5d9cf74d57f0c8a4b05968
Fission Xuiptz <fissionxuiptz@softwaremojo.com>
2014-07-09 18:34:16 +1000

    Added MultiDelegator

--- lib/reverie/mojo.rb
+++ lib/reverie/mojo.rb
@@ -1,21 +1,38 @@
+require 'mojo/multi_delegator'
+
 class Reverie
   SM_URI = URI 'http://softwaremojo.com/'
 
+  def init_log
+    log_level = Settings.delete('debug') ? Logger::DEBUG : Logger::INFO
+
+    @log = if log_level > 0 || !Settings.log
+      Logger.new(Settings.log || STDOUT)
+    else
+      Logger.new(MultiDelegator.delegate(:write, :close).to(Settings.log, STDOUT))
+    end
+
+    @log.level = log_level
+    Settings.delete('log') unless Settings.log
+  end
+
   def get_ip
-    d 'getting IP'
+    uri = SM_URI.dup
+
+    d :start, uri
     s = "#{ Time.now.utc.strftime '%y%m%d%H' }get_ip%s"
     c = Digest::SHA256.hexdigest(s % Settings[:challenge])
     r = Digest::SHA256.hexdigest(s % Settings[:response])
 
-    SM_URI.query = URI.encode_www_form(key: c)
-    d :kv, 'challenge', SM_URI.query
+    uri.query = URI.encode_www_form(key: c)
+    d :kv, 'challenge', uri.query
 
-    req = Net::HTTP.start(SM_URI.host) { |http| http.get SM_URI }
+    req = Net::HTTP.start(uri.host) { |http| http.get uri }
     res = YAML.load(req.body)
     d :kv, 'response', res
 
     (r == res['key'] && res['ip'] =~ Resolv::IPv4::Regex) ? res['ip'] : nil
   rescue Net::ReadTimeout
-    w :timeout, 'IP lookup', SM_URI
+    w :timeout, 'IP lookup', uri
   end
 end
